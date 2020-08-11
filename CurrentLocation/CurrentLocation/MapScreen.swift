109aaa696e8dc4fdc16da246ae8236b436f031ce
Puneet Mahali <iospuneet@gmail.com>
2020-08-11 21:45:59 +0200

    Add App Icon & Bug fixing

--- CurrentLocation/CurrentLocation/MapScreen.swift
+++ CurrentLocation/CurrentLocation/MapScreen.swift
@@ -78,3 +78,59 @@ extension MapScreen: CLLocationManagerDelegate {
         checkLocationAuthorization()
     }
 }
+
+
+
+/* MARK: Alternate Method
+import UIKit
+import MapKit
+class MapScreen: UIViewController {
+     //MARK: - IBOutlets
+     @IBOutlet weak var mapView: MKMapView!
+     //MARK: - Variables
+     fileprivate let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
+     }()
+     //MARK: - UIViewController life Cycle Methods
+     override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpMapView()
+     }
+     
+     //MARK: - Setup Methods
+     func setUpMapView() {
+        mapView.showsUserLocation = true
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        currentLocation()
+     }
+     
+     //MARK: - Helper Method
+     func currentLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if #available(iOS 11.0, *) {
+           locationManager.showsBackgroundLocationIndicator = true
+        } else {
+           // Fallback on earlier versions
+        }
+        locationManager.startUpdatingLocation()
+     }
+}
+//MARK: - CLLocationManagerDelegate Methods
+extension MapScreen: CLLocationManagerDelegate {
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+     }
+     
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+     }
+}
+*/
